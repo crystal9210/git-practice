ad83cbea9f7b2191853dbda0dfa1511c325046d5
sekiyuu <sy9210sss@yahoo.co.jp>
2023-05-15 19:58:38 +0900

    [add]main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b) {
+    if(a<0) exit(1);
+    if(b<0) exit(1);
+    return a + b;
+}
+
+int main(){
+    cout<<'HEllo Wold!'<<endl;
+    return 0;
+}
